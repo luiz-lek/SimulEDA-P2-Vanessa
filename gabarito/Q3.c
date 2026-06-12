3cf55456870e09ee3b4f04e93f3915a272041047
3ricz7 <emdias@id.uff.br>
2026-06-12 00:11:37 -0300

    Q3

--- gabarito/Q3.c
+++ gabarito/Q3.c
@@ -1,3 +1,39 @@
 //
-// Created by luizao on 11/06/2026.
+// Created by Eric on 11/06/2026.
 //
+
+#include "Respostas.h"
+
+int* inver_vet(int* vet, int tam) {
+    if (!vet) return NULL;
+    int temp;
+    for (int i = 0; i < tam/2; i++) {
+        temp = vet[i];
+        vet[i] = vet[tam - i - 1];
+        vet[tam - i - 1] = temp;
+    }
+    return vet;
+}
+
+TNoB** inver_ps(TNoB** a, int tam) {
+    if (!a) return NULL;
+
+    TNoB* aux = NULL;
+    for (int i = 0; i < tam; i++) {
+        aux = a[i];
+        a[i] = a[tam - i];
+        a[tam - i] = aux;
+    }
+    return a;
+}
+
+TNoB* inverte(TNoB *a) {
+    if (!a) return NULL;
+
+    a->s = inver_vet(a->s, a->m);
+    a->p = inver_ps(a->p, a->m);
+
+    for (int i = 0; i < a->m; i++) a->p[i] = inverte(a->p[i]);
+
+    return a;
+}
